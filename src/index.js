48d2547b441609a588dd28fe465aefa340bda3ac
anbreaker <antunez19@gmail.com>
2020-05-18 21:48:15 +0200

    agregada opcion de puerto variable...

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const ejs = require('ejs');
 
 // Settings
 // Inicializacion del Nº de puerto
-const numPuerto = app.set('port', 3000);
+const numPuerto = app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
